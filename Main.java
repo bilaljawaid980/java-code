21967a89d3702ef0da5393a0eb8adc1ad127f24a
BILAL JAWAID <121331714+bilaljawaid980@users.noreply.github.com>
2023-12-26 19:44:39 +0000

    Add files via upload
    Abstraction in java

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+cat a= new cat();
+a.makeSound();
+a.colour="white";
+a.attack();
+a.place();
+
+	}
+
+}
